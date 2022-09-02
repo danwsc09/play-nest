730e741b63d50dab33c575a9e5a5fc1d9b1fbf5c
Wonsang <temporaryaion1@hotmail.com>
2022-09-02 12:32:16 +0900

    movie service

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+  @Get()
+  getAll() {
+    return 'this will return all movies';
+  }
+
+  @Get('/search')
+  search(@Query('year') searchingYear: string) {
+    console.log('qq', searchingYear);
+    return `we are searching for movies after year: ${searchingYear}`;
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') movieId: string) {
+    return `this will return a movie with id: ${movieId}`;
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    console.log(movieData);
+    return movieData;
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return `this will delete a movie with id: ${movieId}`;
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
+  }
+}
